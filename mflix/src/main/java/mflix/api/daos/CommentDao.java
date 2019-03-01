706dae54fea949c3546ef8f54fadd92ee122f9c4
mateusz almannai <m.almannai@crefopay.de>
2019-03-01 15:16:50 +0100

    Error Handling

--- mflix/src/main/java/mflix/api/daos/CommentDao.java
+++ mflix/src/main/java/mflix/api/daos/CommentDao.java
@@ -1,18 +1,16 @@
 package mflix.api.daos;
 
+import mflix.api.models.Comment;
+import mflix.api.models.Critic;
+
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoWriteException;
-import com.mongodb.ReadConcern;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Sorts;
 import com.mongodb.client.model.Updates;
 import com.mongodb.client.result.DeleteResult;
 import com.mongodb.client.result.UpdateResult;
-import mflix.api.models.Comment;
-import mflix.api.models.Critic;
 import org.bson.Document;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
@@ -23,7 +21,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -31,7 +28,9 @@ import java.util.Date;
 import java.util.List;
 
 import static com.mongodb.client.model.Accumulators.sum;
-import static com.mongodb.client.model.Aggregates.*;
+import static com.mongodb.client.model.Aggregates.group;
+import static com.mongodb.client.model.Aggregates.limit;
+import static com.mongodb.client.model.Aggregates.sort;
 import static com.mongodb.client.model.Sorts.descending;
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
@@ -39,154 +38,183 @@ import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 @Component
 public class CommentDao extends AbstractMFlixDao {
 
-  public static String COMMENT_COLLECTION = "comments";
+	public static String COMMENT_COLLECTION = "comments";
 
-  private MongoCollection<Comment> commentCollection;
+	private MongoCollection<Comment> commentCollection;
 
-  private CodecRegistry pojoCodecRegistry;
+	private CodecRegistry pojoCodecRegistry;
 
-  private final Logger log;
+	private final Logger log;
 
-  @Autowired
-  public CommentDao(
-      MongoClient mongoClient, @Value("${spring.mongodb.database}") String databaseName) {
-    super(mongoClient, databaseName);
-    log = LoggerFactory.getLogger(this.getClass());
-    this.db = this.mongoClient.getDatabase(MFLIX_DATABASE);
-    this.pojoCodecRegistry =
-        fromRegistries(
-            MongoClientSettings.getDefaultCodecRegistry(),
-            fromProviders(PojoCodecProvider.builder().automatic(true).build()));
-    this.commentCollection =
-        db.getCollection(COMMENT_COLLECTION, Comment.class).withCodecRegistry(pojoCodecRegistry);
-  }
+	@Autowired
+	public CommentDao(
+			MongoClient mongoClient, @Value("${spring.mongodb.database}") String databaseName) {
+		super(mongoClient, databaseName);
+		log = LoggerFactory.getLogger(this.getClass());
+		this.db = this.mongoClient.getDatabase(MFLIX_DATABASE);
+		this.pojoCodecRegistry =
+				fromRegistries(
+						MongoClientSettings.getDefaultCodecRegistry(),
+						fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+		this.commentCollection =
+				db.getCollection(COMMENT_COLLECTION, Comment.class).withCodecRegistry(pojoCodecRegistry);
+	}
 
-  /**
-   * Returns a Comment object that matches the provided id string.
-   *
-   * @param id - comment identifier
-   * @return Comment object corresponding to the identifier value
-   */
-  public Comment getComment(String id) {
-    return commentCollection.find(new Document("_id", new ObjectId(id))).first();
-  }
+	/**
+	 * Returns a Comment object that matches the provided id string.
+	 *
+	 * @param id - comment identifier
+	 * @return Comment object corresponding to the identifier value
+	 */
+	public Comment getComment(String id) {
+		return commentCollection.find(new Document("_id", new ObjectId(id))).first();
+	}
 
-  /**
-   * Adds a new Comment to the collection. The equivalent instruction in the mongo shell would be:
-   *
-   * <p>db.comments.insertOne({comment})
-   *
-   * <p>
-   *
-   * @param comment - Comment object.
-   * @throw IncorrectDaoOperation if the insert fails, otherwise
-   * returns the resulting Comment object.
-   */
-  public Comment addComment(Comment comment) {
-    if (comment.getId() == null || comment.getId().isEmpty()) {
-      throw new IncorrectDaoOperation("Comment objects need to have an ID field set.");
-    }
+	/**
+	 * Adds a new Comment to the collection. The equivalent instruction in the mongo shell would be:
+	 *
+	 * <p>db.comments.insertOne({comment})
+	 *
+	 * <p>
+	 *
+	 * @param comment - Comment object.
+	 * @throw IncorrectDaoOperation if the insert fails, otherwise
+	 * returns the resulting Comment object.
+	 */
+	public Comment addComment(Comment comment) {
+		if (comment.getId() == null || comment.getId().isEmpty()) {
+			throw new IncorrectDaoOperation("Comment objects need to have an ID field set.");
+		}
 
-    // DONE> Ticket - Update User reviews: implement the functionality that enables adding a new
-    // comment.
-    commentCollection.insertOne(comment);
-    // TODO> Ticket - Handling Errors: Implement a try catch block to
-    // handle a potential write exception when given a wrong commentId.
+		// DONE> Ticket - Update User reviews: implement the functionality that enables adding a new
+		// comment.
+		// DONE> Ticket - Handling Errors: Implement a try catch block to
+		// handle a potential write exception when given a wrong commentId.
 
-    return comment;
-  }
+		try {
+			commentCollection.insertOne(comment);
+			return comment;
+		} catch (MongoWriteException e) {
+			String errorMessage =
+					MessageFormat.format(
+							"Error occurred while adding a new Comment `{}`: {}", comment, e.getMessage());
+			throw new IncorrectDaoOperation(errorMessage);
+		}
+	}
 
-  /**
-   * Updates the comment text matching commentId and user email. This method would be equivalent to
-   * running the following mongo shell command:
-   *
-   * <p>db.comments.update({_id: commentId}, {$set: { "text": text, date: ISODate() }})
-   *
-   * <p>
-   *
-   * @param commentId - comment id string value.
-   * @param text - comment text to be updated.
-   * @param email - user email.
-   * @return true if successfully updates the comment text.
-   */
-  public boolean updateComment(String commentId, String text, String email) {
+	/**
+	 * Updates the comment text matching commentId and user email. This method would be equivalent to
+	 * running the following mongo shell command:
+	 *
+	 * <p>db.comments.update({_id: commentId}, {$set: { "text": text, date: ISODate() }})
+	 *
+	 * <p>
+	 *
+	 * @param commentId - comment id string value.
+	 * @param text      - comment text to be updated.
+	 * @param email     - user email.
+	 * @return true if successfully updates the comment text.
+	 */
+	public boolean updateComment(String commentId, String text, String email) {
 
-    // DONE> Ticket - Update User reviews: implement the functionality that enables updating an
-    // user own comments
-    Bson filter = Filters.and(
-            Filters.eq("email", email),
-            Filters.eq("_id", new ObjectId(commentId)));
-    Bson update = Updates.combine(
-            Updates.set("text", text),
-            Updates.set("date", new Date()));
-    UpdateResult result = commentCollection.updateOne(filter, update);
+		// DONE> Ticket - Update User reviews: implement the functionality that enables updating an
+		// user own comments
+		Bson filter = Filters.and(
+				Filters.eq("email", email),
+				Filters.eq("_id", new ObjectId(commentId)));
+		Bson update = Updates.combine(
+				Updates.set("text", text),
+				Updates.set("date", new Date()));
+		try {
 
-    if (result.getMatchedCount() > 0) {
-      if (result.getModifiedCount() != 1) {
-        log.warn("Comment `{}` text was not updated. Is it the same text?");
-      }
-      return true;
-    }
-    // TODO> Ticket - Handling Errors: Implement a try catch block to
-    // handle a potential write exception when given a wrong commentId.
-    log.error("Could not update comment `{}`. Make sure the comment is owned by `{}`", commentId, email);
-    return false;
-  }
+			UpdateResult res = commentCollection.updateOne(filter, update);
 
-  /**
-   * Deletes comment that matches user email and commentId.
-   *
-   * @param commentId - commentId string value.
-   * @param email - user email value.
-   * @return true if successful deletes the comment.
-   */
-  public boolean deleteComment(String commentId, String email) {
-    if (commentId == null || commentId.isEmpty()) {
-      throw new IllegalArgumentException("CommentID is required.");
-    }
-    // DONE> Ticket Delete Comments - Implement the method that enables the deletion of a user
-    // comment
-    // TIP: make sure to match only users that own the given commentId
-    Bson filter = Filters.and(
-            Filters.eq("_id", new ObjectId(commentId)),
-            Filters.eq("email", email)
-    );
+			if (res.getMatchedCount() > 0) {
 
-    DeleteResult res = commentCollection.deleteOne(filter);
+				if (res.getModifiedCount() != 1) {
+					log.warn("Comment `{}` text was not updated. Is it the same text?");
+				}
 
-    // in case the delete count is different than one the document
-    // either does not exist or it does not match the email + _id filter.
-    if (res.getDeletedCount()!=1){
-      log.warn("Not able to delete comment `{}` for user `{}`. User" +
-                      " does not own comment or already deleted!",
-              commentId, email);
-      return false;
-    }
-    return true;
-    // TODO> Ticket Handling Errors - Implement a try catch block to
-    // handle a potential write exception when given a wrong commentId.
-  }
+				return true;
+			}
+			log.error(
+					"Could not update comment `{}`. Make sure the comment is owned by `{}`",
+					commentId,
+					email);
+			return false;
 
-  /**
-   * Ticket: User Report - produce a list of users that comment the most in the website. Query the
-   * `comments` collection and group the users by number of comments. The list is limited to up most
-   * 20 commenter.
-   *
-   * @return List {@link Critic} objects.
-   */
-  public List<Critic> mostActiveCommenters() {
-    List<Critic> mostActive = new ArrayList<>();
-    // // TODO> Ticket: User Report - execute a command that returns the
-    // // list of 20 users, group by number of comments. Don't forget,
-    // // this report is expected to be produced with an high durability
-    // // guarantee for the returned documents. Once a commenter is in the
-    // // top 20 of users, they become a Critic, so mostActive is composed of
-    // // Critic objects.
-    List<Bson> pipeline = Arrays.asList(
-            group("$email", sum("count", 1L)),
-            sort(descending("count")),
-            limit(20));
-    commentCollection.aggregate(pipeline, Critic.class).iterator().forEachRemaining(mostActive::add);
-    return mostActive;
-  }
+		} catch (MongoWriteException e) {
+			String messageError =
+					MessageFormat.format(
+							"Error occurred while updating comment `{}`: {}", commentId, e.getMessage());
+			throw new IncorrectDaoOperation(messageError);
+		}
+		// DONE> Ticket - Handling Errors: Implement a try catch block to
+		// handle a potential write exception when given a wrong commentId.
+	}
+
+	/**
+	 * Deletes comment that matches user email and commentId.
+	 *
+	 * @param commentId - commentId string value.
+	 * @param email     - user email value.
+	 * @return true if successful deletes the comment.
+	 */
+	public boolean deleteComment(String commentId, String email) {
+		if (commentId == null || commentId.isEmpty()) {
+			throw new IllegalArgumentException("CommentID is required.");
+		}
+		// DONE> Ticket Delete Comments - Implement the method that enables the deletion of a user
+		// comment
+		// TIP: make sure to match only users that own the given commentId
+		Bson filter = Filters.and(
+				Filters.eq("_id", new ObjectId(commentId)),
+				Filters.eq("email", email)
+		);
+
+		// in case the delete count is different than one the document
+		// either does not exist or it does not match the email + _id filter.
+		try {
+			DeleteResult res = commentCollection.deleteOne(filter);
+			if (res.getDeletedCount() != 1) {
+				log.warn(
+						"Not able to delete comment `{}` for user `{}`. User"
+								+ " does not own comment or already deleted!",
+						commentId,
+						email);
+				return false;
+			}
+			return true;
+		} catch (MongoWriteException e) {
+			String errorMessage =
+					MessageFormat.format("Error deleting comment " + "`{}`: {}", commentId, e);
+			throw new IncorrectDaoOperation(errorMessage);
+		}
+		// DONE> Ticket Handling Errors - Implement a try catch block to
+		// handle a potential write exception when given a wrong commentId.
+
+	}
+
+	/**
+	 * Ticket: User Report - produce a list of users that comment the most in the website. Query the
+	 * `comments` collection and group the users by number of comments. The list is limited to up most
+	 * 20 commenter.
+	 *
+	 * @return List {@link Critic} objects.
+	 */
+	public List<Critic> mostActiveCommenters() {
+		List<Critic> mostActive = new ArrayList<>();
+		// // TODO> Ticket: User Report - execute a command that returns the
+		// // list of 20 users, group by number of comments. Don't forget,
+		// // this report is expected to be produced with an high durability
+		// // guarantee for the returned documents. Once a commenter is in the
+		// // top 20 of users, they become a Critic, so mostActive is composed of
+		// // Critic objects.
+		List<Bson> pipeline = Arrays.asList(
+				group("$email", sum("count", 1L)),
+				sort(descending("count")),
+				limit(20));
+		commentCollection.aggregate(pipeline, Critic.class).iterator().forEachRemaining(mostActive::add);
+		return mostActive;
+	}
 }
--- mflix/src/main/java/mflix/api/daos/UserDao.java
+++ mflix/src/main/java/mflix/api/daos/UserDao.java
@@ -1,17 +1,17 @@
 package mflix.api.daos;
 
+import mflix.api.models.Session;
+import mflix.api.models.User;
+
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoWriteException;
 import com.mongodb.WriteConcern;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.model.InsertOneOptions;
 import com.mongodb.client.model.UpdateOptions;
 import com.mongodb.client.model.Updates;
 import com.mongodb.client.result.DeleteResult;
 import com.mongodb.client.result.UpdateResult;
-import mflix.api.models.Session;
-import mflix.api.models.User;
 import org.bson.Document;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
@@ -21,153 +21,192 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-
 import java.text.MessageFormat;
 import java.util.Map;
 
-import static com.mongodb.client.model.Filters.eq;
-import static com.mongodb.client.model.Updates.set;
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 @Configuration
 public class UserDao extends AbstractMFlixDao {
 
-  private final MongoCollection<User> usersCollection;
-  //DONE> Ticket: User Management - do the necessary changes so that the sessions collection
-  //returns a Session object
-  private final MongoCollection<Session> sessionsCollection;
+	private final MongoCollection<User> usersCollection;
+	//DONE> Ticket: User Management - do the necessary changes so that the sessions collection
+	//returns a Session object
+	private final MongoCollection<Session> sessionsCollection;
 
-  private final Logger log;
+	private final Logger log;
 
-  @Autowired
-  public UserDao(
-      MongoClient mongoClient, @Value("${spring.mongodb.database}") String databaseName) {
-    super(mongoClient, databaseName);
-    CodecRegistry pojoCodecRegistry =
-        fromRegistries(
-            MongoClientSettings.getDefaultCodecRegistry(),
-            fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+	@Autowired
+	public UserDao(
+			MongoClient mongoClient, @Value("${spring.mongodb.database}") String databaseName) {
+		super(mongoClient, databaseName);
+		CodecRegistry pojoCodecRegistry =
+				fromRegistries(
+						MongoClientSettings.getDefaultCodecRegistry(),
+						fromProviders(PojoCodecProvider.builder().automatic(true).build()));
 
-    usersCollection = db.getCollection("users", User.class).withCodecRegistry(pojoCodecRegistry);
-    log = LoggerFactory.getLogger(this.getClass());
-    //DONE> Ticket: User Management - implement the necessary changes so that the sessions
-    // collection returns a Session objects instead of Document objects.
-    sessionsCollection = db.getCollection("sessions", Session.class).withCodecRegistry(pojoCodecRegistry);
-  }
+		usersCollection = db.getCollection("users", User.class).withCodecRegistry(pojoCodecRegistry);
+		log = LoggerFactory.getLogger(this.getClass());
+		//DONE> Ticket: User Management - implement the necessary changes so that the sessions
+		// collection returns a Session objects instead of Document objects.
+		sessionsCollection = db.getCollection("sessions", Session.class).withCodecRegistry(pojoCodecRegistry);
+	}
 
-  /**
-   * Inserts the `user` object in the `users` collection.
-   *
-   * @param user - User object to be added
-   * @return True if successful, throw IncorrectDaoOperation otherwise
-   */
-  public boolean addUser(User user) {
-    //DONE > Ticket: Durable Writes -  you might want to use a more durable write concern here!
-    usersCollection.withWriteConcern(WriteConcern.ACKNOWLEDGED.withW(1)).insertOne(user);
-    return true;
-    //TODO > Ticket: Handling Errors - make sure to only add new users
-    // and not users that already exist.
+	/**
+	 * Inserts the `user` object in the `users` collection.
+	 *
+	 * @param user - User object to be added
+	 * @return True if successful, throw IncorrectDaoOperation otherwise
+	 */
+	public boolean addUser(User user) {
+		//DONE > Ticket: Durable Writes -  you might want to use a more durable write concern here!
+		try {
+			usersCollection.withWriteConcern(WriteConcern.MAJORITY).insertOne(user);
+			return true;
 
-  }
+		} catch (MongoWriteException e) {
+			log.error(
+					"Could not insert `{}` into `users` collection: {}", user.getEmail(), e.getMessage());
+			throw new IncorrectDaoOperation(
+					MessageFormat.format("User with email `{0}` already exists", user.getEmail()));
+		}
+		//DONE > Ticket: Handling Errors - make sure to only add new users
+		// and not users that already exist.
 
-  /**
-   * Creates session using userId and jwt token.
-   *
-   * @param userId - user string identifier
-   * @param jwt - jwt string token
-   * @return true if successful
-   */
-  public boolean createUserSession(String userId, String jwt) {
-    //DONE> Ticket: User Management - implement the method that allows session information to be
-    // stored in it's designated collection.
-    Session session = new Session();
-    session.setUserId(userId);
-    session.setJwt(jwt);
-    sessionsCollection.insertOne(session);
-    return true;
-    //TODO > Ticket: Handling Errors - implement a safeguard against
-    // creating a session with the same jwt token.
-  }
+	}
 
-  /**
-   * Returns the User object matching the an email string value.
-   *
-   * @param email - email string to be matched.
-   * @return User object or null.
-   */
-  public User getUser(String email) {
-    User user = null;
-    //DONE> Ticket: User Management - implement the query that returns the first User object.
-    return usersCollection.find(new Document("email", email)).first();
-  }
+	/**
+	 * Creates session using userId and jwt token.
+	 *
+	 * @param userId - user string identifier
+	 * @param jwt    - jwt string token
+	 * @return true if successful
+	 */
+	public boolean createUserSession(String userId, String jwt) {
+		//DONE> Ticket: User Management - implement the method that allows session information to be
+		// stored in it's designated collection.
+		try {
+			Bson updateFilter = new Document("user_id", userId);
+			Bson setUpdate = Updates.set("jwt", jwt);
+			UpdateOptions options = new UpdateOptions().upsert(true);
+			sessionsCollection.updateOne(updateFilter, setUpdate, options);
+			return true;
+		} catch (MongoWriteException e) {
+			String errorMessage =
+					MessageFormat.format(
+							"Unable to $set jwt token in sessions collection: {}", e.getMessage());
+			throw new IncorrectDaoOperation(errorMessage, e);
+		}
+		//DONE > Ticket: Handling Errors - implement a safeguard against
+		// creating a session with the same jwt token.
+	}
 
-  /**
-   * Given the userId, returns a Session object.
-   *
-   * @param userId - user string identifier.
-   * @return Session object or null.
-   */
-  public Session getUserSession(String userId) {
-    //DONE> Ticket: User Management - implement the method that returns Sessions for a given
-    // userId
-    return sessionsCollection.find(new Document("user_id", userId)).first();
-  }
+	/**
+	 * Returns the User object matching the an email string value.
+	 *
+	 * @param email - email string to be matched.
+	 * @return User object or null.
+	 */
+	public User getUser(String email) {
+		User user = null;
+		//DONE> Ticket: User Management - implement the query that returns the first User object.
+		return usersCollection.find(new Document("email", email)).first();
+	}
 
-  public boolean deleteUserSessions(String userId) {
-    //DONE> Ticket: User Management - implement the delete user sessions method
-    DeleteResult deleteResult = sessionsCollection.deleteMany(new Document("user_id", userId));
+	/**
+	 * Given the userId, returns a Session object.
+	 *
+	 * @param userId - user string identifier.
+	 * @return Session object or null.
+	 */
+	public Session getUserSession(String userId) {
+		//DONE> Ticket: User Management - implement the method that returns Sessions for a given
+		// userId
+		return sessionsCollection.find(new Document("user_id", userId)).first();
+	}
 
-    return deleteResult.wasAcknowledged();
-  }
+	public boolean deleteUserSessions(String userId) {
+		//DONE> Ticket: User Management - implement the delete user sessions method
+		DeleteResult deleteResult = sessionsCollection.deleteMany(new Document("user_id", userId));
 
-  /**
-   * Removes the user document that match the provided email.
-   *
-   * @param email - of the user to be deleted.
-   * @return true if user successfully removed
-   */
-  public boolean deleteUser(String email) {
-    // remove user sessions
-    //DONE> Ticket: User Management - implement the delete user method
-    DeleteResult deleteUserResult = usersCollection.deleteOne(new Document("email", email));
-    DeleteResult deleteSessionsResult = sessionsCollection.deleteMany(new Document("user_id", email));
-    //TODO > Ticket: Handling Errors - make this method more robust by
-    // handling potential exceptions.
-    return deleteUserResult.wasAcknowledged() && deleteSessionsResult.wasAcknowledged();
-  }
+		return deleteResult.wasAcknowledged();
+	}
 
-  /**
-   * Updates the preferences of a user identified by `email` parameter.
-   *
-   * @param email - user to be updated email
-   * @param userPreferences - set of preferences that should be stored and replace the existing
-   *     ones. Cannot be set to null value
-   * @return User object that just been updated.
-   */
-  public boolean updateUserPreferences(String email, Map<String, ?> userPreferences) {
-    //DONE> Ticket: User Preferences - implement the method that allows for user preferences to
-    // be updated.
-    if (userPreferences == null) {
-      throw new IncorrectDaoOperation("userPreferences cannot be set to null");
-    }
+	/**
+	 * Removes the user document that match the provided email.
+	 *
+	 * @param email - of the user to be deleted.
+	 * @return true if user successfully removed
+	 */
+	public boolean deleteUser(String email) {
+		// remove user sessions
+		//DONE> Ticket: User Management - implement the delete user method
+		// remove user sessions
+		try {
+			if (deleteUserSessions(email)) {
+				Document userDeleteFilter = new Document("email", email);
+				DeleteResult res = usersCollection.deleteOne(userDeleteFilter);
 
-    Bson queryFilter = new Document("email", email);
-    Bson updateObject = Updates.set("preferences", userPreferences);
+				if (res.getDeletedCount() < 0) {
+					log.warn("User with `email` {} not found. Potential concurrent operation?!");
+				}
 
-    final UpdateResult updateResult = usersCollection.updateOne(
-            queryFilter,
-            updateObject
+				return res.wasAcknowledged();
+			}
+		} catch (Exception e) {
+			String errorMessage = MessageFormat.format(
+					"Issue caught while trying to delete user `{}`: {}",
+					email,
+					e.getMessage());
+			throw new IncorrectDaoOperation(errorMessage);
 
-    );
-    if (updateResult.getModifiedCount() < 1) {
-      log.warn("User `{}` was not updated. Trying to rewrite the same `preferences` field: `{}`",
-              email, userPreferences);
-    }
+		}
+		return false;
+		//DONE > Ticket: Handling Errors - make this method more robust by
+		// handling potential exceptions.
+	}
 
-    //        usersCollection.updateOne(user);
-    //TODO > Ticket: Handling Errors - make this method more robust by
-    // handling potential exceptions when updating an entry.
-    return updateResult.isModifiedCountAvailable();
-  }
+	/**
+	 * Updates the preferences of a user identified by `email` parameter.
+	 *
+	 * @param email           - user to be updated email
+	 * @param userPreferences - set of preferences that should be stored and replace the existing
+	 *                        ones. Cannot be set to null value
+	 * @return User object that just been updated.
+	 */
+	public boolean updateUserPreferences(String email, Map<String, ?> userPreferences) {
+		//DONE> Ticket: User Preferences - implement the method that allows for user preferences to
+		// be updated.
+		if (userPreferences == null) {
+			throw new IncorrectDaoOperation("userPreferences cannot be set to null");
+		}
+
+		// create query filter and update object.
+		Bson updateFilter = new Document("email", email);
+		Bson updateObject = Updates.set("preferences", userPreferences);
+		try {
+			// update one document matching email.
+			UpdateResult res = usersCollection.updateOne(updateFilter, updateObject);
+			if (res.getModifiedCount() < 1) {
+				log.warn(
+						"User `{}` was not updated. Trying to re-write the same `preferences` field: `{}`",
+						email,
+						userPreferences);
+			}
+			return true;
+		} catch (MongoWriteException e) {
+			String errorMessage =
+					MessageFormat.format(
+							"Issue caught while trying to update user `{}`: {}",
+							email,
+							e.getMessage());
+			throw new IncorrectDaoOperation(errorMessage);
+		}
+
+		//        usersCollection.updateOne(user);
+		//DONE Ticket: Handling Errors - make this method more robust by
+		// handling potential exceptions when updating an entry.
+		//    return updateResult.isModifiedCountAvailable();
+	}
 }
